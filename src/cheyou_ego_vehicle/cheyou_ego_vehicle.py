c8e2f1482fa9f4168731007cd34ef1f8fd9d9e6b
co-driver <li.wang@co-driver.ai>
2019-04-30 12:33:52 +0800

    disable not worked ue4_map fix

--- src/cheyou_ego_vehicle/cheyou_ego_vehicle.py
+++ src/cheyou_ego_vehicle/cheyou_ego_vehicle.py
@@ -43,10 +43,14 @@ class CheYou_EgoVehicle(CarlaEgoVehicle):
             "/tf",
             TFMessage, self.teleport_command_updated)
         
-        self.__transformer = tf.Transformer(True,rospy.Duration(10.0))
+        self.__listener = tf.TransformListener()
 
         #这个变量用来保存ego_vehicle的初始生成位置（特别的，在Z轴方向，是实际的位置，而不是指定的位置）
-        self.__InitialPosition = carla.Transform()        
+        self.__InitialPosition = carla.Transform()   
+        
+        self._rate = rospy.Rate(30.0)	    
+        self._br = tf.TransformBroadcaster() 
+    
 
     def restart(self):
         super(CheYou_EgoVehicle,self).restart()
@@ -60,63 +64,87 @@ class CheYou_EgoVehicle(CarlaEgoVehicle):
                                                                   self.__InitialPosition.rotation.yaw))      
         #禁止物理模拟，防止平移和静止的时候的车辆上下晃动
         self.player.set_simulate_physics(False)  
+	    #Set autopilot to output throttle and steer
+        self.player.set_autopilot(False)
         
-        #添加UE4_map坐标系
-        self.add_UE4_transform()
-
     #这个函数在ROS的TF树中增加一个新的Frame,新的Frame为UE4地图坐标（右手系），它和map的相对位置和姿态就是小车在UE4地图中的
     #初始位置和姿态
     def add_UE4_transform(self):
-        m = geometry_msgs.msg.TransformStamped()
-        m.header.frame_id = 'UE4_map'
-        m.child_frame_id = 'map'
-        m.transform.translation.x = self.__InitialPosition.location.x
+        x = self.__InitialPosition.location.x
+        z = self.__InitialPosition.location.z
         #将初始值变为右手系
-        m.transform.translation.y = -self.__InitialPosition.location.y
-        m.transform.translation.z = self.__InitialPosition.location.z
+        y = -self.__InitialPosition.location.y
+
         quat = quaternion_from_euler(
                 math.radians(float(self.__InitialPosition.rotation.roll)), 
                 math.radians(float(self.__InitialPosition.rotation.pitch)),
+                #将初始值变为右手系
                 math.radians(-float(self.__InitialPosition.rotation.yaw)) )
-        m.transform.rotation.x = quat[0]
-        m.transform.rotation.y=  quat[1]
-        m.transform.rotation.z=  quat[2]
-        m.transform.rotation.w = quat[3]
-        self.__transformer.setTransform(m)
+
+        self._br.sendTransform((x,y,z),
+                         (quat[0],quat[1],quat[2],quat[3]),
+                         rospy.Time.now(),
+                         "map",
+                         "UE4_map")
+	
 
     def teleport_command_updated(self,ros_TFMessage):   
         try:
             #cartographer发布的/tf消息，我们定义了两个坐标系 map->base_link，base_link是车身坐标系 
             #我们又增加了一个新的坐标系UE4_map,base_link和UE4_map之间的相对关系才是我们需要的
-            trans =self.__transformer.lookupTransform('UE4_map','base_link',rospy.Time())
-            carla_trans = carla.Transform()
-            
-            carla_trans.location.x = trans.transform.translation.x
-            #UE4使用左手系, Y方向与ROS系统坐标系方向相反
-            carla_trans.location.y = - trans.transform.translation.y
-            #当前我们的环境假定是二维平面，不要更新在Z方向的数据
-            carla_trans.location.z =  self.__InitialPosition.location.z
-            
-            quaternion = (
-                trans.rotaion.x,
-                trans.rotaion.y,
-                trans.rotaion.z,
-                trans.rotaion.w
-            )
-            _, _, yaw = euler_from_quaternion(quaternion)
-            #UE4使用左手系, 偏航方向与ROS系统的偏航方向正好相反
-            carla_trans.rotation.yaw = - math.degrees(yaw)
-            
-            #当前我们的环境假定是二维平面，不要更新roll和pitch
-            carla_trans.rotation.roll = 0.0
-            carla_trans.rotation.pitch = 0.0 
-
-          
-            if self.player: 
-                self.player.set_transform(carla_trans)         
-                rospy.loginfo("Teleport vehicle to x=%f y=%f z=%f", carla_trans.location.x, carla_trans.location.y, carla_trans.location.z) 
+            (trans,rot) = self.__listener.lookupTransform('map', 'base_link', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             rospy.loginfo("CheYou: tf transform exception!")
+            return
+
+
+        #trans =self.__transformer.lookupTransform('UE4_map','base_link',rospy.Time())
+        carla_trans = carla.Transform()
+            
+        carla_trans.location.x = trans[0] + self.__InitialPosition.location.x
+        #UE4使用左手系, Y方向与ROS系统坐标系方向相反, be careful the self.__InitialPosition.location.y is left hand coordinate value
+        carla_trans.location.y = - (trans[1]-self.__InitialPosition.location.y)
+        #当前我们的环境假定是二维平面，不要更新在Z方向的数据
+        carla_trans.location.z =  self.__InitialPosition.location.z
+            
+        quaternion = (
+            rot[0],
+            rot[1],
+            rot[2],
+            rot[3]
+        )
+        _, _, yaw = euler_from_quaternion(quaternion)
+        #UE4使用左手系, 偏航方向与ROS系统的偏航方向正好相反
+        carla_trans.rotation.yaw = - math.degrees(yaw)
+            
+        #当前我们的环境假定是二维平面，不要更新roll和pitch
+        carla_trans.rotation.roll = 0.0
+        carla_trans.rotation.pitch = 0.0 
+
+          
+        if self.player: 
+            self.player.set_transform(carla_trans)         
+            rospy.loginfo("Teleport vehicle to x=%f y=%f z=%f", carla_trans.location.x, carla_trans.location.y, carla_trans.location.z) 
+        
+    
+    def run(self):
+        """
+        main loop
+        """
+        client = carla.Client(self.host, self.port)
+        client.set_timeout(2.0)
+        self.world = client.get_world()
+        self.restart()
+
+        try:
+            rospy.spin()
+        except rospy.ROSInterruptException:
+            pass
+
+#        while not rospy.is_shutdown():
+#            self.add_UE4_transform()
+#            self._rate.sleep()
+
 
 # ==============================================================================
 # -- main() --------------------------------------------------------------------
